1769fd5972e47f601f7e27f60670b97ecd362251
Manny Chacon <chmanuel2030@gmail.com>
2021-06-04 00:52:03 -0700

    app.js and header/main component

---
+++ employee-directory/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+    return (
+        <div className="bg-light header">
+            <h2>Employee Directory</h2>
+        </div>
+    );
+}
+
+export default Header;
---
+++ employee-directory/src/components/Main/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import Header from "../Header";
+import SearchBox from "../SearchBox";
+import DataTable from "../DataTable";
+import DataBody from "../DataBody";
+import API from "../../utils/API";
+
+class Main extends Component {
+    state = {
+        search: "",
+        results: []
+    };
+
+    componentDidMount() {
+        this.getEmployees();
+    };
+
+    getEmployees = () => {
+        API.search()
+            .then(res => this.setState({ results: res.data.results }))
+            .catch(err => console.log(err));
+    };
+
+    handleInputChange = event => {
+        let value = event.target.value;
+        const results = this.state.results;
+
+        const searchResults = results.filter((searchedName) => searchedName.name.first.includes(value))
+
+        this.setState({
+            search: value,
+            results: searchResults
+        })
+    };
+
+    handleSort = event => {
+        event.preventDefault();
+        const results = this.state.results;
+
+        const sortedResults = results.sort((a, b) =>
+            a.name.first.localeCompare(b.name.first));
+
+        this.setState({
+            results: sortedResults
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <SearchBox
+                    name="search"
+                    value={this.state.search}
+                    handleInputChange={this.handleInputChange} />
+                <div className="container">
+                <table className="table table-striped">
+                    <DataTable handleSort={this.handleSort} />
+                    <DataBody
+                        results={this.state.results}
+                    />
+                </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default Main;
